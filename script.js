363bddb87eacbd29327aba0e02424c3ad0706882
Prithiraj Nandan <prithirajnandan@gmail.com>
2020-06-14 00:31:52 +0530

    Total Users

--- script.js
+++ script.js
@@ -28,4 +28,32 @@ function closeForm() {
 
     $("#call-num").css("opacity", "1");
     $("#reg").css("opacity", "1");
-  }
+}
+
+(function manageTotalUsers() {
+
+    let totalUsers = String(getRandom(100000, 500000));
+    totalUsers = addComma(totalUsers);
+
+    addToTotalUsers(totalUsers);
+
+    function addToTotalUsers(currentValue) {
+        let num = Number(currentValue.replace(",", ""));
+        num++;
+        num = String(num);
+        num = addComma(num);
+        $("#total-users").html(num);
+        setTimeout(function () {
+            addToTotalUsers(num);
+        }, getRandom(400, 2000));
+    }
+
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    function addComma(num) {
+        return num.slice(0, 3) + "," + num.slice(3, 6);
+
+    }
+
+})();
